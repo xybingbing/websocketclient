94fcabebc87be5edef85f5b0e077af5b49cc5a55
xybingbing <xybingbing@live.com>
2021-08-16 17:39:34 +0800

    添加测试

--- websocketclient_test.go
+++ websocketclient_test.go
@@ -3,6 +3,9 @@ package websocketclient
 import "testing"
 
 func TestClient(t *testing.T) {
-
-
+	client := WebSocketClient{}
+	client.Connect("ws://localhost:8080/echo")
+	client.push("hello world")
+	var msg = client.recv()
+	t.Log(msg)
 }
